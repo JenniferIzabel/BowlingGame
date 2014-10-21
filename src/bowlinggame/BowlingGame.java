c0a5c98eead5af9e49fb3aef2a1322fda6948267
Jennifer <izabeljennifer@gmail.com>
2014-10-21 19:13:20 -0200

    adicionados os arquivos

---
+++ src/bowlinggame/BowlingGame.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bowlinggame;
+
+/**
+ *
+ * @author a1136976
+ */
+public class BowlingGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    private int jogada = 0;
+    private int pontos[] = new int[21]; 
+            
+
+    public int getScore() {
+        int somaTotal = 0;
+        for (int i = 0; i < pontos.length-1; i++) {
+            somaTotal += pontos[i];
+            
+            if(i != 0 && i%2 == 1){
+                int somaRodada = 0;
+                somaRodada += pontos[i];
+                somaRodada += pontos[i-1];
+                if(somaRodada == 10){
+                    somaTotal += pontos[i+1];
+                }
+            }    
+        }
+        return somaTotal;
+    }
+
+    public void roll(int i) {
+        pontos[jogada] = i;
+       
+        if(jogada%2 == 0 && i == 10){
+            pontos[i+1] = 0;
+            jogada ++;
+        }
+         jogada ++;
+        
+    }
+    
+   
+}
---
+++ test/BowlingGameTest.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import bowlinggame.BowlingGame;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author a1136976
+ */
+public class BowlingGameTest {
+    
+    public BowlingGameTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    
+     @Test
+     public void inicio() {
+         BowlingGame bowlingGame = new BowlingGame();
+         assertEquals(0, bowlingGame.getScore());
+     }
+     
+     @Test
+     public void jodada1(){
+         BowlingGame bowlingGame = new BowlingGame();
+         bowlingGame.roll(4);
+         bowlingGame.roll(5);
+         assertEquals(9, bowlingGame.getScore());
+         
+     }
+     
+     @Test
+     public void totalSimples(){
+         BowlingGame bowlingGame = new BowlingGame();
+         for (int i = 0; i < 20; i++) {
+            bowlingGame.roll(1);
+         }
+         assertEquals(20, bowlingGame.getScore());
+         
+     }
+     
+     
+     @Test
+     public void totalSpare(){
+         BowlingGame bowlingGame = new BowlingGame();
+         bowlingGame.roll(5);
+         bowlingGame.roll(5);
+         for (int i = 2; i < 20; i++) {
+            bowlingGame.roll(1);
+         }
+         assertEquals(29, bowlingGame.getScore());
+         
+     }
+     
+     @Test
+     public void totalStrike(){
+         BowlingGame bowlingGame = new BowlingGame();
+         bowlingGame.roll(10);
+         for (int i = 1; i < 20; i++) {
+            bowlingGame.roll(1);
+         }
+         assertEquals(30, bowlingGame.getScore());
+         
+     }
+}
