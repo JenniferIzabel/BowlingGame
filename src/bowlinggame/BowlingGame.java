78bad1cbd5b5a3e6fa3e46166e57106bd70ac4fe
a1136976 <a1136976@03e104-507870>
2014-11-11 20:54:16 -0200

    Spare e Strike resolvido

--- src/bowlinggame/BowlingGame.java
+++ src/bowlinggame/BowlingGame.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package bowlinggame;
 
 /**
@@ -18,38 +17,42 @@ public class BowlingGame {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
     private int jogada = 0;
-    private int pontos[] = new int[21]; 
-            
+    private int pontos[] = new int[21];
 
     public int getScore() {
         int somaTotal = 0;
-        for (int i = 0; i < pontos.length-1; i++) {
+        for (int i = 0; i < pontos.length - 1; i++) {
             somaTotal += pontos[i];
             
-            if(i != 0 && i%2 == 1){
+            if (pontos[i] == 10) {
+                int somaRodada = 0;
+                somaRodada += pontos[i + 1];
+                somaRodada += pontos[i + 2];
+                somaTotal += somaRodada;
+
+            } else if (i % 2 == 1) {
                 int somaRodada = 0;
                 somaRodada += pontos[i];
-                somaRodada += pontos[i-1];
-                if(somaRodada == 10){
-                    somaTotal += pontos[i+1];
+                somaRodada += pontos[i - 1];
+                if (somaRodada == 10) {
+                    somaTotal += pontos[i + 1];
                 }
-            }    
+            }
         }
         return somaTotal;
     }
 
     public void roll(int i) {
         pontos[jogada] = i;
-       
-        if(jogada%2 == 0 && i == 10){
-            pontos[i+1] = 0;
-            jogada ++;
+
+        if (i == 10) {
+            pontos[jogada + 1] = 0;
+            jogada++;
         }
-         jogada ++;
-        
+        jogada++;
+
     }
-    
-   
+
 }
--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -14,78 +14,78 @@ import org.junit.Test;
 
 /**
  *
- * @author a1136976
+ * @author a1136976 - Jennifer
  */
 public class BowlingGameTest {
-    
+
     public BowlingGameTest() {
     }
     
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
-    
-     @Test
-     public void inicio() {
-         BowlingGame bowlingGame = new BowlingGame();
-         assertEquals(0, bowlingGame.getScore());
-     }
-     
-     @Test
-     public void jodada1(){
-         BowlingGame bowlingGame = new BowlingGame();
-         bowlingGame.roll(4);
-         bowlingGame.roll(5);
-         assertEquals(9, bowlingGame.getScore());
-         
-     }
-     
-     @Test
-     public void totalSimples(){
-         BowlingGame bowlingGame = new BowlingGame();
-         for (int i = 0; i < 20; i++) {
+    @Test
+    public void inicio() {
+        BowlingGame bowlingGame = new BowlingGame();
+        assertEquals(0, bowlingGame.getScore());
+    }
+
+    @Test
+    public void jodada1() {
+        BowlingGame bowlingGame = new BowlingGame();
+        bowlingGame.roll(4);
+        bowlingGame.roll(5);
+        assertEquals(9, bowlingGame.getScore());
+
+    }
+
+    @Test
+    public void totalSimples() {
+        BowlingGame bowlingGame = new BowlingGame();
+        for (int i = 0; i < 20; i++) {
             bowlingGame.roll(1);
-         }
-         assertEquals(20, bowlingGame.getScore());
-         
-     }
-     
-     
-     @Test
-     public void totalSpare(){
-         BowlingGame bowlingGame = new BowlingGame();
-         bowlingGame.roll(5);
-         bowlingGame.roll(5);
-         for (int i = 2; i < 20; i++) {
+        }
+        assertEquals(20, bowlingGame.getScore());
+
+    }
+
+    @Test
+    public void totalSpare() {
+        BowlingGame bowlingGame = new BowlingGame();
+        bowlingGame.roll(5);
+        bowlingGame.roll(5);
+        for (int i = 2; i < 20; i++) {
             bowlingGame.roll(1);
-         }
-         assertEquals(29, bowlingGame.getScore());
-         
-     }
-     
-     @Test
-     public void totalStrike(){
-         BowlingGame bowlingGame = new BowlingGame();
-         bowlingGame.roll(10);
-         for (int i = 1; i < 20; i++) {
+        }
+        assertEquals(29, bowlingGame.getScore());
+
+    }
+
+    @Test
+    public void totalStrike() {
+        BowlingGame bowlingGame = new BowlingGame();
+        bowlingGame.roll(2);
+        bowlingGame.roll(2);
+        bowlingGame.roll(10);
+        for (int i = 1; i < 17; i++) {
             bowlingGame.roll(1);
-         }
-         assertEquals(30, bowlingGame.getScore());
-         
-     }
+        }
+        assertEquals(32, bowlingGame.getScore());
+
+    }
 }
